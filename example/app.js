e654881806824b8c600c4441e55d3b3f9347c11c
pselle <pamela.selle@gmail.com>
2015-04-22 15:44:23 -0400

    Updating example app and prez

--- example/app.js
+++ example/app.js
@@ -2,6 +2,31 @@ var output = document.getElementById('output')
 var button = document.getElementById('continue')
 button.addEventListener('click', nextValue)
 
+var foo = 'John went to the store and said "Hey there " "Another thing" to the clerk'
+
+function* StateParser(str) {
+  var acc = ""
+  var amInQuote = false
+  for (charIndex in str) {
+    var character = str[charIndex]
+    if (character === " " && !amInQuote){
+        yield acc
+        acc = ""
+        continue
+    }
+
+    acc = acc + character
+
+    if(character == '"'){
+        amInQuote = !amInQuote
+    }
+  }
+  if(acc != ""){
+    yield acc
+  }
+  return "\000"
+}
+
 function* MyGenerator() {
   console.log('something')
   yield(this)
@@ -10,10 +35,13 @@ function* MyGenerator() {
   console.log('something else entirely')
 }
 
-var myGenerator = MyGenerator()
+var myGenerator = StateParser(foo)
 
 function nextValue() {
   var p = document.createElement("p");
-  p.innerText = "Generator done: " + myGenerator.next().done
+  //p.innerText = "Generator done: " + myGenerator.next().done
+  p.innerText = myGenerator.next().value
   output.appendChild(p);
+
+//  for (x in StateParser())
 }
